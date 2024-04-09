1c7d6f2c0c106beb25b58b482b9ec98d3debaa66
thiagomartin <155474906+thiagomartin@users.noreply.github.com>
2024-04-09 14:56:39 -0300

    feat estudo algoritimo

---
+++ lib/algoritimo.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "algoritimo/version"
+
+module Algoritimo
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/algoritimo_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Algoritimo do
+  it "has a version number" do
+    expect(Algoritimo::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
